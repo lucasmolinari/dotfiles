721a46b1ac406c35e1d5d2acc33ae96d47ff5f3c
Lucas Molinari <101225122+lucasmolinari@users.noreply.github.com>
2024-05-28 09:51:46 -0300

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,29 @@
 return {
-  { "sainnhe/gruvbox-material" },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      commentStyle = { italic = true },
+      keywordStyle = { italic = true },
+      terminalColors = true,
+      background = {
+        dark = "dragon",
+        light = "lotus",
+      },
+    },
+  },
+  {
+    "jacoborus/tender.vim",
+  },
+  {
+    "sainnhe/gruvbox-material",
+  },
+  {
+    "savq/melange-nvim",
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "melange",
     },
   },
 }
