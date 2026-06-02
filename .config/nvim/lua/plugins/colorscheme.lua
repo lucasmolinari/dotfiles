0db3f036358250ca252053f803ab553182f50d55
Lucas Molinari <lucas.molinari2003@gmail.com>
2026-06-02 15:06:00 -0300

    feat(nvim): add colorschemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 return {
+  { "ellisonleao/gruvbox.nvim", name = "gruvbox" },
   { "ayu-theme/ayu-vim", name = "ayu" },
+  { "stevedylandev/darkmatter-nvim", name = "darkmatter" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "ayu",
+      colorscheme = "gruvbox",
     },
   },
 }
